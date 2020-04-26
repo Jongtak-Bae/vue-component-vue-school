5c14a98b2c70eea241eb1cd944a11051ec7f4805
Jongtak Bae <peizhengze@gmail.com>
2020-04-26 17:42:47 -0400

    update

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-let planComponent={
+let PlanComponent = {
   template: '#plan-template',
   props: {
     name: {
       type: String,
       required: true
+    },
+    selectedPlan: {
+      type: String
     }
   },
-  methods:{
-    select(){
-      return console.log('selected')
+  computed: {
+    isSelected() {
+      return this.name === this.selectedPlan
+    }
+  },
+  methods: {
+    select() {
+      this.$emit('select', this.name)
+    }
   }
 }
-}
 
-let planPickerComponent={
-  template:'#plan-picker-template',
-  components:{
-    plan:planComponent
+let PlanPickerComponent = {
+  template: '#plan-picker-template',
+  components: {
+    plan: PlanComponent
   },
-  data(){
+  data() {
     return {
-      plans: ['The Single', 'The Curious', 'The Addict']
+      plans: ['The Single', 'The Curious', 'The Addict'],
+      selectedPlan: null
+    }
+  },
+  methods: {
+    selectPlan(plan) {
+      this.selectedPlan = plan
     }
   }
 }
 
-
-
-
-
- new Vue({
-   el: '#app',
-   components:{
-     'plan-picker':planPickerComponent
-   }
-
- })
+new Vue({
+  el: '#app',
+  components: {
+    'plan-picker': PlanPickerComponent
+  }
+})
